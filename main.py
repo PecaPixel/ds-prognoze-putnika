3ac4ac494040983ebb2d5a34833068a94315254e
Predrag Andjelkovic <predragandjelkovic@live.com>
2025-08-12 00:19:19 +0200

    Dorade: evaluacija R², unos godina, prognoza; čišćenje .pyc preko .gitignore

--- main.py
+++ main.py
@@ -1,55 +1,42 @@
+from models.linear_model import train_linear_model, evaluate_train_linear_model
 import pandas as pd
 import numpy as np
-from models.linear_model import train_linear_model, evaluate_model
-from models.exp_model import train_exp_model, evaluate_exp_model
+import math
 
 df = pd.read_csv("data/tblTokoviPutnikaZelecnice.csv")
-print("============================================================================")
-print("Kolone u fajlu: ")
+print("===================učitavanje tabele i pregled imena svih kolona=====================")
 print(df.columns)
-print("============================================================================")
+print("=====================================================================================")
 
-#Priprema podataka u smislu odabira potrebnih kolona
-X = df[["Godina"]] #ovo uzimam da mi je kolona Godina X promenjiva i ona u ovom slucaju ima 11 redova tj 11 godina
+#Priprema podataka za upotrebu
+X = df[["Godina"]]
 y = df["Obim H-START"]
+#Učenje modela sa konkretnim podacima
+model_1 = train_linear_model(X, y)
+#Ocena modela na konkretnim podacima Obim H-START za postojeće godine
+y_pred_model_1, mae, r2 = evaluate_train_linear_model(model_1, X, y)
+print(f"Srenja apsolutna greška iznosi = {math.ceil(mae):,}")
+print(f"Koeficijent korelacije iznosi = {r2:.4f}")
+print("====================Ocena kvaliteta modela===========================================")
+if 0 <= r2 < 0.50:
+    print(f"r2 je {r2:.4f} u granicam 0 <= r2 < 0.50. To znači da je loš za prognozu (potraži bolji model!)")
+elif 0.50 <= r2 < 0.75:
+    print("Model je osrednji. Postoji prostora za poboljšanje!")
+elif 0.75 <= r2 < 0.90:
+    print("Model je dobar za prognozu!")
+elif 0.90 <= r2 < 1:
+    print("Model je izuzetno dobar!")
+print("=====================================================================================")
+print("Prognoza za buduće unete godine na osnovu naučenog modela X - godine i y - obim prevoza")
+unos = input("Unesi nove buduće godine za prognozu (npr. 2020, 2021, 2022 itd: )")
+nove_godine_1 = [int(godine.strip()) for godine in unos.split(",")]
+print(f"Unede godine za prognozu su: {nove_godine_1}")
+x_nove = pd.DataFrame(nove_godine_1, columns=["Godina"])
+y_predict_nove_god = model_1.predict(x_nove)
+y_pred_nove_ceil = np.ceil(y_predict_nove_god).astype(int)
+#Kreiranje novog data frejma za prognozirane godine
+df_prognozirano = pd.DataFrame({"Godina": nove_godine_1, "Prognozirane vrednosti": y_pred_nove_ceil})
 
-#Treniranje modela
-prvi_model = train_linear_model(X, y)
-
-#Evaluacija modela
-y_pred, mse, r2 = evaluate_model(prvi_model, X, y)
-
- #Ispis rezultata
-print("\nRezultati modela: ")
-print(f"   -MSE: {mse:.2f}")
-print(f"   -R2: {r2: .4f}")
-
-#Predikcija za novu godinu
-unos = input("Unesi godine za koje želiš predikciju: ")
-godine_za_predikciju = [[int(godina.strip())] for godina in unos.split(",")]
-nova_godina = pd.DataFrame(godine_za_predikciju, columns=["Godina"])
-predikcija= prvi_model.predict(nova_godina)
-print("============================================================================")
-for godina, vrednost in zip(nova_godina["Godina"], predikcija):
-    print(f"Predviđanje za {godina}: {int(vrednost)} putnika")
-print("============================================================================")
-
-#Treniranje modela Exp
-model2 = train_exp_model(X, y)
-
-#Evaluacija Exp modela
-y_exp_pred, mae_exp, r2_exp = evaluate_exp_model(model2, X, y)
-
-#Ispis rezultata
-print("\nRezultati Exp modela: ")
-print(f"    MAE: {mae_exp: .2f}")
-print(f"    R2: {r2_exp: .4f}")
-
-#Predikcija za nove kodine pomoću Exponencijalnog moela
-exp_predikcija_log = model2.predict(nova_godina)
-exp_predikcija = np.exp(exp_predikcija_log)
-print("============================================================================")
-print("Predikcije eksp. modela za nove godine:")
-for godina, vrednost in zip(nova_godina["Godina"], exp_predikcija):
-    print(f"Exp predviđanje za {godina}: {int(vrednost)} putnika")
-print("============================================================================")
+print("====================Prognoza za unete godine=========================================")
+print(df_prognozirano.to_string(index=False))
+print("=====================================================================================")
--- models/linear_model.py
+++ models/linear_model.py
@@ -2,13 +2,16 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, r2_score
 
 def train_linear_model(x, y):
-    model = LinearRegression()
-    model.fit(x, y)
+    train_model1 = LinearRegression()
+    train_model1.fit(x, y)
     
-    return model
-def evaluate_model(model, x, y):
-    y_pred = model.predict(x)
-    mse = mean_absolute_error(y, y_pred)
-    r2 = r2_score(y, y_pred)
+    return train_model1
+
+def evaluate_train_linear_model(model: LinearRegression, x, y):
+    y_predict = model.predict(x)
+    mae = mean_absolute_error(y, y_predict)
+    r2 = r2_score(y, y_predict)
     
-    return y_pred, mse, r2
+    return y_predict, mae, r2
+
+#Funkcije za predikciju (prognozu) je završena.. PecaPixel :D
